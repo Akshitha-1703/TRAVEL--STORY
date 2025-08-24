4fce8179c453a42f9210662a994b367f02032eba
Akshitha-1703 <akshitha.katta17@gmail.com>
2025-08-24 10:53:27 +0530

    first-commit

---
+++ Frontend/travel-story-app/src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+import LOGO from "../assets/images/logo.svg";
+import ProfileInfo from "./Cards/ProfileInfo";
+import { useNavigate } from "react-router-dom";
+import SearchBar from "./Input/SearchBar";
+
+const Navbar = ({ userInfo, searchQuery, setSearchQuery, onSearchNote, handleClearSearch}) => {
+  const isToken = localStorage.getItem("token");
+  const navigate = useNavigate();
+
+  const onLogout = ()=>{
+    localStorage.clear();
+  navigate("/login");
+  };
+
+  const handleSearch = ()=>{
+    if (searchQuery) {
+      onSearchNote(searchQuery);
+    }
+  };
+
+  const onClearSearch = ()=>{
+    handleClearSearch();
+    setSearchQuery("");
+  };
+
+
+
+  return (
+    <div className="h-14 bg-white flex items-center justify-between px-6 py-2 shadow sticky top-0 z-10">
+      <img src={LOGO} alt="travel story" className="h-14 w-auto object-contain" />
+
+      {isToken && (
+      <>
+
+
+          <SearchBar
+            value={searchQuery}
+            onChange={({ target }) => {
+              setSearchQuery(target.value);
+            }}
+            handleSearch = {handleSearch}
+            onClearSearch = {onClearSearch}
+          />
+          <ProfileInfo userInfo={userInfo} onLogout={onLogout} />{" "}
+      </>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
